549742be98f8330b5d7c152266c620498d65c219
Vinícius Cavalcante <viniciusdeabreucavalcante@gmail.com>
2022-01-18 22:51:28 -0300

    add for practice

---
+++ for-practice.py
@@ -0,0 +1,12 @@
+screen_number = 30
+print('# for iterating over a list:')
+sentence = ['I', 'want', 'to', 'eat', 'a', 'burger']
+
+for word in sentence:
+    print(word)
+
+print('-' * screen_number)
+print('# for with range')
+for number in range(5, 35, 5):
+    print(number)
+    
