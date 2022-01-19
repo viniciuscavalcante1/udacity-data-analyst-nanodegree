f1e57e378dba99c751659afc3fa285e19e9f9786
Vinícius Cavalcante <viniciusdeabreucavalcante@gmail.com>
2022-01-18 23:11:25 -0300

    test

--- for-practice.py
+++ for-practice.py
@@ -1,12 +1,28 @@
-screen_number = 30
+def screen_split(screen_number=30):
+    print('-' * screen_number)
+
+
+screen_split()
 print('# for iterating over a list:')
 sentence = ['I', 'want', 'to', 'eat', 'a', 'burger']
 
 for word in sentence:
     print(word)
 
-print('-' * screen_number)
+screen_split()
 print('# for with range')
 for number in range(5, 35, 5):
     print(number)
-    
+
+
+screen_split()
+print('# create usernames')
+names = ['Vinícius Cavalcante', 'Linda Cavalcante', 'Elvis Cavalcante']
+usernames = []
+
+for name in names:
+    usernames.append(name.lower().replace(' ', '_'))
+
+print(usernames)
+
+screen_split()
