6cbffa82affc8e89d4ef2f88abdca95fa81db3de
Vinícius Cavalcante <viniciusdeabreucavalcante@gmail.com>
2022-01-18 23:34:55 -0300

    add more for exercises

--- for-practice.py
+++ for-practice.py
@@ -1,4 +1,4 @@
-def screen_split(screen_number=30):
+def screen_split(screen_number=80):
     print('-' * screen_number)
 
 
@@ -8,6 +8,7 @@ sentence = ['I', 'want', 'to', 'eat', 'a', 'burger']
 
 for word in sentence:
     print(word)
+
 
 screen_split()
 print('# for with range')
@@ -25,4 +26,34 @@ for name in names:
 
 print(usernames)
 
+
 screen_split()
+print('# modify usernames')
+for i in range(len(usernames)):
+    usernames[i] = usernames[i].title().replace('_', ' ')
+
+print(usernames)
+
+
+screen_split()
+print('# tag-counter')
+tokens = ['<greeting>', 'Hello, World!', '</greeting>']
+count = 0
+
+for token in tokens:
+    if token[0] == '<' and token[-1] == '>':
+        count += 1
+
+print(count)
+
+
+screen_split()
+print('# create an html list')
+items = ['first string', 'second string']
+html_str = '<ul>\n'
+
+for item in items:
+    html_str += '<li>{}</li>\n'.format(item)
+html_str += '</ul>'
+
+print(html_str)
