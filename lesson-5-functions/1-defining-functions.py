4950730480c64555a048b1943493de1ecf2035de
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2022-01-20 15:59:12 -0300

    add 1-defining-functions.py

---
+++ lesson-5-functions/1-defining-functions.py
@@ -0,0 +1,13 @@
+def cylinder_volume(height, radius=5):
+    pi = 3.14159
+    return height * pi * radius ** 2
+
+
+print(cylinder_volume(10, 3))
+
+
+def print_greeting():
+    print('Hello, World!')
+
+
+print(cylinder_volume(height=10, radius=7))
