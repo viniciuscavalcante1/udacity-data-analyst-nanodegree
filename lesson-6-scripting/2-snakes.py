2ff88cf92f5dbe7fedda01b5e158f4d6d3f75dbd
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2022-01-20 21:56:32 -0300

    add 2-snakes.py

---
+++ lesson-6-scripting/2-snakes.py
@@ -0,0 +1,16 @@
+how_many_snakes = 5
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3
+"""
+
+
+print(snake_string * how_many_snakes)
