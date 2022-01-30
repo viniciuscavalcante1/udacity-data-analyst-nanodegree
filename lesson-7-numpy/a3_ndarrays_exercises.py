ce86676727604e0dee18eef445eb4dc91d2ac3ed
Vinícius Cavalcante <vcavalcanteab@gmail.com>
2022-01-30 17:29:03 -0300

    Add a3_ndarrays_exercises.py

---
+++ lesson-7-numpy/a3_ndarrays_exercises.py
@@ -0,0 +1,20 @@
+print("Quiz: ABC's")
+
+"""Create a numpy array of strings containing letters 'a' through 'j' (inclusive)
+ of the alphabet. Then, use numpy array attributes to print
+  the following information about this array:
+
+dtype of array
+shape of array
+size of array
+"""
+
+import numpy as np
+letter_array = np.array(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'])
+print("Letter Array: ", letter_array)
+
+print("Array dtype: ", letter_array.dtype)
+
+print("Array shape: ", letter_array.shape)
+
+print("Array size: ", letter_array.size)
